3cab4ec6b735f4dde01d49138920a4740fc46c02
gjdame <gregoryjdame@gmail.com>
2018-07-30 20:29:25 +0000

    remove /

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,13 +5,12 @@ from datetime import datetime
 import os
 
 env.hosts = ['localhost']
-env.reject_interactive_password = True
 
 def do_pack():
     n = datetime.now()
     name = "web_static_{}{}{}{}{}{}.tgz".format(n.year, n.month\
 , n.day, n.hour, n.minute, n.second)
-    local('mkdir -p /versions')
-    local("tar -cvzf /versions/{} web_static".format(name))
-    size = os.stat("/versions/{}".format(name)).st_size
+    local('mkdir -p versions')
+    local("tar -cvzf versions/{} web_static".format(name))
+    size = os.stat("versions/{}".format(name)).st_size
     print("web_static packed: versions/{} -> {}".format(name, size))
