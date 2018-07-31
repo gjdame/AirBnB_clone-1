78b38d144f14b816dd3a09fad25020597feb9910
gjdame <gregoryjdame@gmail.com>
2018-07-31 01:08:24 +0000

    add 100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+'''fabfile tar packaging'''
+from fabric.api import local, run, env, put, cd
+from datetime import datetime
+import os
+
+env.user = 'ubuntu'
+env.hosts = ['35.237.95.74', '35.185.23.42']
+
+
+def do_pack():
+    '''Packes web_static in tgz format'''
+    n = datetime.now()
+    name = "web_static_{}{}{}{}{}{}.tgz".format(
+        n.year, n.month, n.day, n.hour, n.minute, n.second)
+    local('mkdir -p versions')
+    local("tar -cvzf versions/{} web_static".format(name))
+    size = os.stat("versions/{}".format(name)).st_size
+    print("web_static packed: versions/{} -> {}".format(name, size))
+    path = "versions/{}".format(name)
+    return(path)
+
+
+def do_deploy(archive_path):
+
+    if not archive_path:
+        return(False)
+    name = archive_path.split('/')[1]
+    try:
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+            .format(name, name))
+        run("rm /tmp/{}".format(name))
+        run("mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
+        print("New version deployed")
+        return(True)
+    except BaseException:
+        return(False)
+
+
+def deploy():
+
+    try:
+        path = do_pack()
+    except BaseException:
+        return(False)
+    do_deploy(path)
+
+def do_clean(number=0):
+
+    if number == 0 or number == 1:
+        with cd.local('./versions/'):
+            run('ls -t | tail -n +2 | xargs rm --')
+        with cd.local('/data/web_static/releases/'):
+            run('ls -t | tail -n +2 | xargs rm --')
+    else:
+        with cd.local('./versions/'):
+            run('ls -t | tail -n +2 | xargs rm --')
+        with cd.local('/data/web_static/releases/'):
+            run('ls -t | tail -n +2 | xargs rm --')
