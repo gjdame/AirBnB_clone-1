4d3792e265d193f4e489ab6dbb66753aafb19a43
gjdame <gregoryjdame@gmail.com>
2018-08-15 17:46:39 +0000

    fix #8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,8 @@ from models import storage
 
 app = Flask(__name__)
 app.url_map.strict_slashes=False
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 @app.route('/states_list')
 def display_states():
