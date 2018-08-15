4aadac34a9cd2011ba26576ff1291b504e6ecdb1
gjdame <gregoryjdame@gmail.com>
2018-08-15 18:16:41 +0000

    add #9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,12 +7,17 @@ app = Flask(__name__)
 app.url_map.strict_slashes=False
 
 @app.route('/states')
-def display():
-    return render_template('9-states.html', storage=storage.all('State'))
+@app.route('/states_list')
+def display_states():
+    return render_template('7-states_list.html', storage=storage.all('State'))
 
 @app.route('/states/<id>')
-def display_by_id():
-    return render_template('9-states.html', storage=storage.all('State'))
+def display_by_id(id):
+    state_obj = None
+    for obj in storage.all("State").values():
+        if obj.id == id:
+            state_obj = obj
+    return render_template('9-states.html', storage=storage.all('State'), state_obj=state_obj)
 
 @app.teardown_appcontext
 def close(exception):
