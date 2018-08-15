8c92926abc3f1d7a94fd5c305cbe8f83649dd4c9
gjdame <gregoryjdame@gmail.com>
2018-08-15 21:40:56 +0000

    clean style

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,12 +12,17 @@ app.jinja_env.lstrip_blocks = True
 def display_states():
     return render_template('7-states_list.html', storage=storage.all('State'))
 
+
 @app.route('/cities_by_states')
 def display_cities_by_states():
-    return render_template('8-cities_by_states.html', storage=storage.all('State'))
+    return render_template('8-cities_by_states.html',
+                           storage=storage.all('State'))
+
+
 @app.teardown_appcontext
 def close(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
