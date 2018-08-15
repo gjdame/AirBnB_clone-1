25024fbc40a3f48d589b39d9c382bf76d503ac1d
gjdame <gregoryjdame@gmail.com>
2018-08-15 20:15:00 +0000

    pycode style 1 and 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,14 +3,18 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return('Hello HBNB!')
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return('HBNB')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
